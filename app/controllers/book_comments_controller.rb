4d40459fe6b04d91f652d90c944f063359fac3c3
amiblog <reza.am.0425@gmail.com>
2022-01-28 09:59:08 +0000

    課題③完了時点まで

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,7 +4,8 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to request.referer
+    #redirect_to request.referer
+    @book = Book.find(params[:id])
   end
 
   def destroy
